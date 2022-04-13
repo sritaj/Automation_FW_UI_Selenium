e7455e49bce8a6e20b4533ceba7e0b39f1f5bef3
Sritaj <sritajp@gmail.com>
2022-04-13 18:09:07 +0530

    Added properties file path and getter method to retrieve

--- src/main/java/constants/FrameworkConstants.java
+++ src/main/java/constants/FrameworkConstants.java
@@ -3,4 +3,12 @@ package constants;
 public final class FrameworkConstants {
 
     private FrameworkConstants(){}
+
+    private static final String RESOURCEPATH = System.getProperty("user.dir");
+    private static final String PROPERTIESFILEPATH = RESOURCEPATH + "/src/test/resources/config/config.properties";
+
+    public static String getPropertiesFilePath(){
+        return PROPERTIESFILEPATH;
+    }
+
 }
