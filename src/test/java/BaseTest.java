daf05bf1776f3a495784760466d7c804ee86c4e9
Sritaj <sritajp@gmail.com>
2022-04-13 11:11:32 +0530

    Added BaseTest file to implement Before, After clauses before Tests run

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+
+    @BeforeMethod
+    public void setUp(){
+
+    }
+
+    @AfterMethod
+    public void tearDown(){
+
+    }
+}
