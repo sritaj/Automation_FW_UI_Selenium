1a3be9e85159f22ed07e00d04a6daba917dff5cd
Sritaj <sritajp@gmail.com>
2022-04-13 13:55:43 +0530

    Moved BaseTest to base package

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,15 +1,20 @@
+package base;
+
+import driver.Driver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
 
+    protected BaseTest(){}
+
     @BeforeMethod
     public void setUp(){
-
+        Driver.initDriver();
     }
 
     @AfterMethod
     public void tearDown(){
-
+        Driver.quitDriver();
     }
 }
