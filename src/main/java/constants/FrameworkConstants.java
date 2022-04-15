ef285035b164048980eed0335c42d9f87e3b5c0d
Sritaj <sritajp@gmail.com>
2022-04-15 23:33:46 +0530

    Added JsonConfigFile path

--- src/main/java/constants/FrameworkConstants.java
+++ src/main/java/constants/FrameworkConstants.java
@@ -7,11 +7,16 @@ public final class FrameworkConstants {
     private static final String RESOURCEPATH = System.getProperty("user.dir");
     private static final String PROPERTIESFILEPATH = RESOURCEPATH + "/src/test/resources/config/config.properties";
     private static final int EXPLICITWAITTIMEOUT = 10;
+    private static final String JSONTESTDATAFILEPATH = RESOURCEPATH + "/src/test/resources/config/config.json";
 
     public static String getPropertiesFilePath(){
         return PROPERTIESFILEPATH;
     }
 
     public static int getExplicitWaitTimeout() { return EXPLICITWAITTIMEOUT; }
+
+    public static String getJsonDataFilePath(){
+        return JSONTESTDATAFILEPATH;
+    }
 
 }
