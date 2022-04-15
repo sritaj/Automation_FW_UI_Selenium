2fc0510d1c596a2a24959866997ab652d19b4926
Sritaj <sritajp@gmail.com>
2022-04-15 18:44:37 +0530

    Initialised ElementHelper class in BasePage constructor

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,13 +1,16 @@
 package pages;
 
 import driver.DriverManager;
+import helpers.ElementHelper;
 import org.openqa.selenium.WebDriver;
 
 public class BasePage {
 
-    WebDriver driver;
+    protected WebDriver driver;
+    protected ElementHelper elementHelper;
 
     protected BasePage() {
         this.driver = DriverManager.getDriver();
+        elementHelper = new ElementHelper();
     }
 }
