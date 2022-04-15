5beaff49401b488c1c9d26a3ec4d344aaac1117e
Sritaj <sritajp@gmail.com>
2022-04-15 18:43:53 +0530

    Added ExplicitWait default timeout

--- src/main/java/constants/FrameworkConstants.java
+++ src/main/java/constants/FrameworkConstants.java
@@ -6,9 +6,12 @@ public final class FrameworkConstants {
 
     private static final String RESOURCEPATH = System.getProperty("user.dir");
     private static final String PROPERTIESFILEPATH = RESOURCEPATH + "/src/test/resources/config/config.properties";
+    private static final int EXPLICITWAITTIMEOUT = 10;
 
     public static String getPropertiesFilePath(){
         return PROPERTIESFILEPATH;
     }
+
+    public static int getExplicitWaitTimeout() { return EXPLICITWAITTIMEOUT; }
 
 }
