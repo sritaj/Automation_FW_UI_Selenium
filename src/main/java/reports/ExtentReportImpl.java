97f36dffb5c689a9b253feb92dd396dc1964ebca
Sritaj <sritajp@gmail.com>
2022-04-16 12:39:06 +0530

    Added JavaDoc and screenshot method to pass/skip/fail tests

--- src/main/java/reports/ExtentReportImpl.java
+++ src/main/java/reports/ExtentReportImpl.java
@@ -7,6 +7,7 @@ import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 import constants.FrameworkConstants;
+import utils.TakeScreenshotImpl;
 
 import java.util.Objects;
 
@@ -20,6 +21,9 @@ public final class ExtentReportImpl {
     private ExtentReportImpl() {
     }
 
+    /**
+     * Method to initialize Extent Report
+     */
     public static void initializeReport() {
         if (Objects.isNull(extent)) {
             extent = new ExtentReports();
@@ -31,28 +35,70 @@ public final class ExtentReportImpl {
         }
     }
 
+    /**
+     * Method to set the Extent Report on Test run
+     *
+     * @param testDescription - Test Description from TestNG to append to report
+     * @param testName        - Test Name to append to report
+     */
     public static void startTestExecution(String testDescription, String testName) {
         ExtentReportManager.setTest(extent.createTest(testDescription, testName));
     }
 
-    public static void passTest(String testName) {
+    /**
+     * Method to mark a Test as pass with required info and screenshot
+     *
+     * @param testName             - Test Name to append to info
+     * @param screenshotIsRequired - specifiying if screenshot is required or not
+     */
+    public static void passTest(String testName, String screenshotIsRequired) {
         String passText = "<b>" + testInfo + testName + " PASSED" + "</b>";
         m = MarkupHelper.createLabel(passText, ExtentColor.GREEN);
-        ExtentReportManager.getTest().pass(m);
+        if (screenshotIsRequired.equalsIgnoreCase("yes")) {
+            ExtentReportManager.getTest().pass(m).addScreenCaptureFromBase64String(TakeScreenshotImpl.takeScreenshotAsBase64());
+        } else {
+            ExtentReportManager.getTest().pass(m);
+        }
+
     }
 
-    public static void skipTest(String testName) {
+    /**
+     * Method to mark a Test as skip with required info and screenshot
+     *
+     * @param testName             - Test Name to append to info
+     * @param screenshotIsRequired - specifiying if screenshot is required or not
+     */
+    public static void skipTest(String testName, String screenshotIsRequired) {
         String skipTest = "<b>" + testInfo + testName + " SKIPPED" + "</b>";
         m = MarkupHelper.createLabel(skipTest, ExtentColor.GREY);
-        ExtentReportManager.getTest().skip(m);
+        if (screenshotIsRequired.equalsIgnoreCase("yes")) {
+            ExtentReportManager.getTest().skip(m).addScreenCaptureFromBase64String(TakeScreenshotImpl.takeScreenshotAsBase64());
+        } else {
+            ExtentReportManager.getTest().skip(m);
+        }
+
     }
 
-    public static void failTest(String testName) {
+    /**
+     * Method to mark a Test as fail with required info and screenshot
+     *
+     * @param testName             - Test Name to append to info
+     * @param screenshotIsRequired - specifiying if screenshot is required or not
+     */
+    public static void failTest(String testName, String screenshotIsRequired) {
         String failText = "<b>" + testInfo + testName + " FAILED" + "</b>";
         m = MarkupHelper.createLabel(failText, ExtentColor.RED);
-        ExtentReportManager.getTest().fail(m);
+        if (screenshotIsRequired.equalsIgnoreCase("yes")) {
+            ExtentReportManager.getTest().fail(m).addScreenCaptureFromBase64String(TakeScreenshotImpl.takeScreenshotAsBase64());
+        } else {
+            ExtentReportManager.getTest().fail(m);
+        }
+
     }
 
+    /**
+     * Method to flush Extent Report
+     */
     public static void flushReports() {
         if (Objects.nonNull(extent)) {
             extent.flush();
