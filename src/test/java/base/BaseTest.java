53b118951a1d29f26fed5f9a7403fa1cdd9b85fb
Sritaj <sritajp@gmail.com>
2022-04-16 11:50:47 +0530

    Added BeforeSuite, AfterSuite for extent reports & updated BeforeMethod

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,20 +1,56 @@
 package base;
 
 import driver.Driver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+import reports.ExtentReportImpl;
+
+import java.lang.reflect.Method;
 
 public class BaseTest {
 
-    protected BaseTest(){}
+    protected BaseTest() {
+    }
+
+    @BeforeSuite
+    public void beforeSuite(ITestContext context) {
+
+        // Extent Report Initialization
+        ExtentReportImpl.initializeReport();
+    }
 
     @BeforeMethod
-    protected void setUp(){
+    protected void setUp(Method method) {
+
+        // Extent Report Initialization
+        String testDescription = method.getAnnotation(Test.class).testName();
+        String testName = method.getName();
+        ExtentReportImpl.startTestExecution(testDescription, testName);
+
         Driver.initDriver();
     }
 
     @AfterMethod
-    protected void tearDown(){
+    protected void tearDown(ITestResult result, Method method) {
+
+        if (ITestResult.FAILURE == result.getStatus()) {
+            String testName = result.getName();
+            ExtentReportImpl.failTest(testName);
+
+        } else if (ITestResult.SUCCESS == result.getStatus()) {
+            String testName = result.getName();
+            ExtentReportImpl.passTest(testName);
+
+        } else if (ITestResult.SKIP == result.getStatus()) {
+            String testName = result.getName();
+            ExtentReportImpl.skipTest(testName);
+        }
         Driver.quitDriver();
+    }
+
+    @AfterSuite()
+    public void afterSuite() {
+        ExtentReportImpl.flushReports();
     }
 }
