aa316386c388f573a9f0b09dd45eab5384ae52d1
Sritaj <sritajp@gmail.com>
2022-04-17 00:31:20 +0530

    Added StringDecodeImpl to decode base64 strings

---
+++ src/main/java/utils/StringDecodeImpl.java
@@ -0,0 +1,20 @@
+package utils;
+
+import java.util.Base64;
+
+public final class StringDecodeImpl {
+
+    private StringDecodeImpl() {
+    }
+
+    /**
+     * Method to get Title for specified page
+     *
+     * @param value - The Encoded String as parameter
+     * @return String - The decoded String
+     */
+    public static String stringDecode(String value) {
+
+        return new String(Base64.getDecoder().decode(value));
+    }
+}
