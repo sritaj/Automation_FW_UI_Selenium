cae156ec95aabb22ef793f0ef58410116c281239
Sritaj <sritajp@gmail.com>
2022-04-16 11:48:42 +0530

    Added path to retrieve Extent Reports

--- src/main/java/constants/FrameworkConstants.java
+++ src/main/java/constants/FrameworkConstants.java
@@ -8,6 +8,7 @@ public final class FrameworkConstants {
     private static final String PROPERTIESFILEPATH = RESOURCEPATH + "/src/test/resources/config/config.properties";
     private static final int EXPLICITWAITTIMEOUT = 10;
     private static final String JSONTESTDATAFILEPATH = RESOURCEPATH + "/src/test/resources/config/config.json";
+    private static final String EXTENTREPORTPATH = RESOURCEPATH + "/test-reports/";
 
     public static String getPropertiesFilePath(){
         return PROPERTIESFILEPATH;
@@ -18,5 +19,7 @@ public final class FrameworkConstants {
     public static String getJsonDataFilePath(){
         return JSONTESTDATAFILEPATH;
     }
+
+    public static String getExtentReportPath() {return  EXTENTREPORTPATH; }
 
 }
