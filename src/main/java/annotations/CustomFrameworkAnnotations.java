fb6433739058dfea7c72b3b7513236f56ddbbd80
Sritaj <sritajp@gmail.com>
2022-04-16 23:33:00 +0530

    Added Custom Annotations to be used in the Tests

---
+++ src/main/java/annotations/CustomFrameworkAnnotations.java
@@ -0,0 +1,17 @@
+package annotations;
+
+import enums.TestCaseType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface CustomFrameworkAnnotations {
+
+    TestCaseType testCaseType();
+    String[] testCaseModule() default "";
+}
+
