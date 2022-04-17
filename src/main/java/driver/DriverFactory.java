76474887c966c6c3428a8e961a84f927555d7f78
Sritaj <sritajp@gmail.com>
2022-04-17 13:38:12 +0530

    Added Driver factory to set the Driver based on Local or Remote run
    Check point to set the Runmode and Browser based on the Maven parameter or Properties file along with default values to handle NPE

---
+++ src/main/java/driver/DriverFactory.java
@@ -0,0 +1,87 @@
+package driver;
+
+import enums.ConfigProperties;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import utils.PropertiesFileImpl;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Objects;
+
+/**
+ * DriverFactory class to setup WebDriver based on Local or Remote run
+ */
+public final class DriverFactory {
+
+    private DriverFactory() {
+    }
+
+    /**
+     * Method to setup WebDriver for local or remote run and initialising the Browser specified based on Properties file or Maven command
+     *
+     * @return WebDriver - WebDriver object
+     */
+    public static WebDriver getDriver() {
+        WebDriver driver = null;
+        String runmode = PropertiesFileImpl.getDataFromPropertyFile(ConfigProperties.RUNMODE);
+        String url = PropertiesFileImpl.getDataFromPropertyFile(ConfigProperties.HUBURL);
+        String browser = null;
+
+        //Checking if Browser is specified from Maven command, if not Browser to be picked from properties
+        if (System.getProperty("BROWSER") == null) {
+            if (Objects.isNull(PropertiesFileImpl.getDataFromPropertyFile(ConfigProperties.BROWSER))
+                    || PropertiesFileImpl.getDataFromPropertyFile(ConfigProperties.BROWSER).isEmpty()) {
+                browser = "chrome";
+            } else {
+                browser = PropertiesFileImpl.getDataFromPropertyFile(ConfigProperties.BROWSER);
+            }
+        }else{
+            browser = System.getProperty("BROWSER");
+        }
+
+
+        DesiredCapabilities cap;
+        if (browser.equalsIgnoreCase("chrome")) {
+            if (runmode.equalsIgnoreCase("remote")) {
+                cap = new DesiredCapabilities();
+                cap.setBrowserName(Browser.CHROME.browserName());
+
+                try {
+                    driver = new RemoteWebDriver(new URL(url), cap);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+                ChromeOptions chromeOptions = new ChromeOptions();
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver(chromeOptions);
+            }
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            if (runmode.equalsIgnoreCase("remote")) {
+                cap = new DesiredCapabilities();
+                cap.setBrowserName(Browser.FIREFOX.browserName());
+
+                try {
+                    driver = new RemoteWebDriver(new URL(url), cap);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver(firefoxOptions);
+            }
+        }
+        return driver;
+    }
+}
