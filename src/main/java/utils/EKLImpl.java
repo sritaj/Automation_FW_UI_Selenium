c5a77fe3ea5042f1ea597ec68caa995aae68378f
Sritaj <sritajp@gmail.com>
2022-04-20 20:03:27 +0530

    Added EKLImp class 2 create methods to write/delete docs in Elastisearch

---
+++ src/main/java/utils/EKLImpl.java
@@ -0,0 +1,65 @@
+package utils;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+/**
+ * EKLImp class to implement RestAssured Methods for Elastic Search queries
+ */
+public final class EKLImpl {
+
+    private EKLImpl() {
+    }
+
+    /**
+     * Method to create the Request Specification
+     *
+     * @return Request Specification - Request specification for the Rest APIs
+     */
+    public static RequestSpecification setRequestSpec() {
+        RequestSpecification specs = new RequestSpecBuilder()
+                .setBaseUri("http://localhost:9200/")
+                .setBasePath("regression")
+                .build();
+
+        return specs;
+    }
+
+    /**
+     * Method to delete the Regression Collection
+     *
+     */
+    public static void clearPreviousELKResults() {
+        RequestSpecification specs = setRequestSpec();
+        given(specs).delete();
+    }
+
+    /**
+     * Method to set test run data to Regression Collection
+     *
+     * @param testName - Test/Method name
+     * @param status - Test Status
+     */
+    public static void sendResultsToELK(String testName, String status) {
+        Map<String, String> map = new HashMap<>();
+        map.put("testName", testName);
+        map.put("status", status);
+
+        RequestSpecification specs = setRequestSpec();
+        Response response = given(specs).contentType(ContentType.JSON)
+                .log().all()
+                .body(map)
+                .post("/_doc");
+
+        response.then().statusCode(201);
+        response.prettyPrint();
+    }
+
+}
