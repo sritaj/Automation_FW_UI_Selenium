01ab1244cee8b903badde906f1e1a39a52d977f9
QAdam1 <151572570+QAdam1@users.noreply.github.com>
2024-05-11 14:34:03 +0300

    updated selectors, added waits, and modified tests accordingly

--- src/main/java/helpers/ElementHelper.java
+++ src/main/java/helpers/ElementHelper.java
@@ -5,9 +5,14 @@ import enums.WaitStrategy;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.LoggerImpl;
 
+import java.time.Duration;
 import java.util.List;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 
 /**
  * ElementHelper class to create wrappers for WebElement methods with Logging and relevant operations
@@ -15,9 +20,11 @@ import java.util.List;
 public final class ElementHelper {
 
     private WebDriver driver;
+    private WebDriverWait waitDriver;
 
     public ElementHelper() {
         this.driver = DriverManager.getDriver();
+//        waitDriver = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     /**
@@ -26,6 +33,7 @@ public final class ElementHelper {
      * @param by - The locator for the specified element
      */
     public WebElement getElement(By by) {
+//        return waitDriver.until(ExpectedConditions.presenceOfElementLocated(by));
         return driver.findElement(by);
     }
 
@@ -36,6 +44,7 @@ public final class ElementHelper {
      * @param elementInfo - Element which is clicked
      */
     public void clickElement(By by, String elementInfo) {
+//        waitDriver.until(elemen/tToBeClickable(by)).click();
         getElement(by).click();
         LoggerImpl.logSteps("Element clicked : " + elementInfo);
     }
@@ -77,6 +86,20 @@ public final class ElementHelper {
     }
 
     /**
+     * Method to get Text for the specified element
+     *
+     * @param by          - The locator for the specified element
+     * @param waitType    - The WaitType Enum required
+     * @param elementInfo - Element which is clicked
+     * @return String - The title of the specified element
+     */
+    public String getText(By by, WaitStrategy waitType, String elementInfo) {
+        LoggerImpl.logSteps("Get text for : " + elementInfo);
+        return WaitHelper.performExplictiWait(waitType, by).getText();
+
+    }
+
+    /**
      * Method to send input to the Element using the specified locator
      *
      * @param by          - The locator for the specified element
@@ -89,6 +112,19 @@ public final class ElementHelper {
     }
 
     /**
+     * Method to send input to the Element using the specified locator
+     *
+     * @param by          - The locator for the specified element
+     * @param waitType    - The WaitType Enum required
+     * @param elementInfo - Element which is clicked
+     * @param input       - The user input as string for the specified element
+     */
+    public void sendKeys(By by, WaitStrategy waitType, String input, String elementInfo) {
+        LoggerImpl.logSteps("Send input '" + input + "' to : " + elementInfo);
+        WaitHelper.performExplictiWait(waitType, by).sendKeys(input);
+    }
+
+    /**
      * Method to check the Element is displayed using the specified locator
      *
      * @param by - The locator for the specified element
@@ -96,6 +132,17 @@ public final class ElementHelper {
      */
     public boolean elementIsDisplayed(By by) {
         return getElement(by).isDisplayed();
+    }
+
+    /**
+     * Method to check the Element is displayed using the specified locator
+     *
+     * @param by - The locator for the specified element
+     * @param waitType    - The WaitType Enum required
+     * @return boolean - True/False based on the element
+     */
+    public boolean elementIsDisplayed(By by, WaitStrategy waitType) {
+        return WaitHelper.performExplictiWait(waitType, by) != null;
     }
 
     /**
